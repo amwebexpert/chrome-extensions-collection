37199e1da1f23fac7446082ed4136b0fdde6f394
Andre-3rd-Viking <andre@t-b.ca>
2024-06-06 06:53:31 -0400

    feat: content script example

--- coding-guide-helper/public/scripts/content.js
+++ coding-guide-helper/public/scripts/content.js
@@ -1,4 +1,7 @@
-console.info(document.location.href);
+console.info(
+  "Extensions can run scripts that read and modify the content of a page. These are called content scripts. They live in an isolated world, meaning they can make changes to their JavaScript environment without conflicting with their host page or other extensions' content scripts.",
+  document.location.href
+);
 
 // const element = document.querySelector("body");
 // element.style.color = "red";
