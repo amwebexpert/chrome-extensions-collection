3111c11011c875745010e89286a7a121a965e90a
Andre-3rd-Viking <andre@t-b.ca>
2024-06-07 06:48:56 -0400

    chore: protect against missing root

--- coding-guide-helper/src/main.tsx
+++ coding-guide-helper/src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import './main.css'
 
-const root = document.getElementById('root')
-if (root) {
+const main = () => {
+  const root = document.getElementById('root')
+
+  if (!root) {
+    console.error('====>>> "root" element not found in the document.', document.location.href)
+    return
+  }
+
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   )
 }
+
+main()
