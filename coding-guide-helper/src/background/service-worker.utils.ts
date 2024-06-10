e5cf964e400311deab39cef11426e64cce1daea8
Andre-3rd-Viking <andre@t-b.ca>
2024-06-10 16:45:32 -0400

    feat: markdown ToC parsing step 1

--- coding-guide-helper/src/background/service-worker.utils.ts
+++ coding-guide-helper/src/background/service-worker.utils.ts
@@ -1,4 +1,4 @@
-import { marked } from 'marked'
+import { type Tokens, type TokensList, marked } from 'marked'
 import { MenuItems } from '../models/models'
 
 export const fetchCodingGuidelines = async (url: string) => {
@@ -12,4 +12,24 @@ export const menuItemSendSelection: chrome.contextMenus.CreateProperties = {
   id: MenuItems.SEND_SELECTION,
   title: 'Search in Coding Guidelines',
   contexts: ['selection'],
+}
+
+export const getTableOfContent = (tokensList: TokensList): Tokens.List =>
+  tokensList.find((token) => token.type === 'list') as Tokens.List
+
+export const getTableOfContentLinks = (toc: Tokens.List): Tokens.Link[] => {
+  const links: Tokens.Link[] = []
+
+  for (const item of toc.items) {
+    if (item.type !== 'list_item') continue
+
+    const listItem = item as Tokens.ListItem
+    const link = listItem.tokens[0] as Tokens.Text
+
+    if (link.tokens) {
+      links.push(link.tokens[0] as Tokens.Link)
+    }
+  }
+
+  return links
 }
