16cfdd297d34346739e39ffc68a7cedfaeaa03c6
Andre-3rd-Viking <andre@t-b.ca>
2024-06-22 12:34:42 -0400

    test: added fixed coveverage for homeStation

--- coding-guide-helper/src/background/markdown-parser.test.ts
+++ coding-guide-helper/src/background/markdown-parser.test.ts
@@ -1,20 +1,18 @@
-const fs = require("fs");
-const path = require("path");
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 
-import { marked } from "marked";
+import { marked } from 'marked'
 
 describe('markdown parser tests suite', () => {
-
   it('should extract markdown TOC', async () => {
     // arrange
-    const file = path.join(__dirname, "../../", "public/markdowns/example-1.md");
-    const markdownText = fs.readFileSync(file, "utf8", (_err: any, data: any) => data);
+    const file = path.join(__dirname, '../../', 'public/markdowns/example-1.md')
+    const markdownText = fs.readFileSync(file, 'utf8')
 
     // act
     const markdownTokens = marked.lexer(markdownText)
 
     // assert
-    expect(markdownTokens).toBeDefined();
-  });
-
-});
+    expect(markdownTokens.length).toBeGreaterThan(0)
+  })
+})
