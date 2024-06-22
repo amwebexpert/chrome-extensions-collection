2e82a294e31ab3e1f295ad3fdb31f86f5ab85817
Andre-3rd-Viking <andre@t-b.ca>
2024-06-22 12:47:59 -0400

    feat: split TOC from content

--- coding-guide-helper/src/background/markdown-parser.ts
+++ coding-guide-helper/src/background/markdown-parser.ts
@@ -1,15 +1,15 @@
-export const splitTocAndContent = (markdown: string) => {
+export const splitTocAndContent = (markdownText: string) => {
   // regex to split at very first level one title line, starting with "# "
   const regex = /^# .*\n/gm
 
-  const match = regex.exec(markdown)
+  const match = regex.exec(markdownText)
 
   if (!match) {
     throw new Error('No title found in markdown')
   }
 
-  const toc = markdown.slice(0, match.index)
-  const content = markdown.slice(match.index)
+  const toc = markdownText.slice(0, match.index)
+  const content = markdownText.slice(match.index)
 
   return {
     toc,
