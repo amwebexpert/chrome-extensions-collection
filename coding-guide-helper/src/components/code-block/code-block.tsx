23634079d4be63b7031c9fd8231b9ef560769cbb
Andre-3rd-Viking <andre@t-b.ca>
2024-06-27 07:39:14 -0400

    style: code block dark mode support

--- coding-guide-helper/src/components/code-block/code-block.tsx
+++ coding-guide-helper/src/components/code-block/code-block.tsx
@@ -2,12 +2,16 @@ import type { ClassAttributes, FunctionComponent, HTMLAttributes } from 'react'
 import type { ExtraProps } from 'react-markdown'
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coy as style } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { a11yDark as dark, coy as light } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { useDarkTheme } from '../theme/use-dark-theme'
 import { extractLanguageFromClassName } from './code-block.utils'
 
 type Props = ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps
 
 export const CodeBlock: FunctionComponent<Props> = ({ children, className, ...rest }) => {
+  const { isDarkMode } = useDarkTheme()
+  const style = isDarkMode ? dark : light
+
   const language = extractLanguageFromClassName(className)
 
   if (language) {
