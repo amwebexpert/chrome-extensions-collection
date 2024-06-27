408977a697dc0db8a4786317453d0b3e7840931c
Andre-3rd-Viking <andre@t-b.ca>
2024-06-27 07:34:13 -0400

    style: default light theme

--- coding-guide-helper/src/components/theme/use-dark-theme.ts
+++ coding-guide-helper/src/components/theme/use-dark-theme.ts
@@ -1,10 +1,14 @@
 import { useCallback, useEffect, useState } from 'react'
 
+const DEFAULT_VALUE = false
+
 export const useDarkTheme = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [isDarkMode, setIsDarkMode] = useState(DEFAULT_VALUE)
 
   useEffect(() => {
-    chrome.storage.local.get('isDarkMode', ({ isDarkMode }) => setIsDarkMode(isDarkMode ?? false))
+    chrome.storage.local.get('isDarkMode', ({ isDarkMode }) =>
+      setIsDarkMode(isDarkMode ?? DEFAULT_VALUE),
+    )
     chrome.storage.session.onChanged.addListener((changes) => {
       if (changes.isDarkMode) setIsDarkMode(changes.isDarkMode.newValue)
     })
