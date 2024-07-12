f85160ab8615693142f03df8a6804ea9670096c2
Andre-3rd-Viking <andre@t-b.ca>
2024-07-12 05:10:11 -0400

    fix: move port connection into the useEffect hook

--- coding-guide-helper/src/components/panels/search-panel/hooks/use-search.ts
+++ coding-guide-helper/src/components/panels/search-panel/hooks/use-search.ts
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { type GuidelineNode, MessageType, PortName } from '../../../../models/models'
 import { doSearch, doSearchDebounced } from './use-search.utils'
 
-const port = chrome.runtime.connect({ name: PortName.POPUP })
-
 export const useSearch = () => {
+  const portRef = useRef<chrome.runtime.Port>()
+
   const [search, setSearch] = useState('')
   const [isSearching, setIsSearching] = useState(false)
   const [searchResults, setSearchResults] = useState<GuidelineNode[]>([])
@@ -14,7 +14,9 @@ export const useSearch = () => {
     chrome.storage.local.get('search', ({ search }) => setSearch(search ?? ''))
 
     // listen for worker search results and update the state
-    port.onMessage.addListener((message, _port) => {
+    portRef.current = chrome.runtime.connect({ name: PortName.POPUP })
+    portRef.current.onDisconnect.addListener(() => console.info('popup disconnected'))
+    portRef.current.onMessage.addListener((message, _port) => {
       const { type, payload } = message
       if (type === MessageType.ON_SEARCH_COMPLETED) {
         setSearchResults(payload)
