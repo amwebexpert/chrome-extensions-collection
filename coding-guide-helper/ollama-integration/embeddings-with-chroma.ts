4cd1b42b2699a4cc2ae8fd1cf0eea266bdd67178
Andre-3rd-Viking <andre@t-b.ca>
2024-10-27 13:35:53 -0400

    docs: removed react rule from common

--- coding-guide-helper/ollama-integration/embeddings-with-chroma.ts
+++ coding-guide-helper/ollama-integration/embeddings-with-chroma.ts
@@ -1,6 +1,5 @@
 import { collectOnlineGuidelines } from '../src/background/service-worker.utils'
-import type { GuidelineNode } from '../src/models/models'
-import { loadRules } from './../src/ia/utils/guideline.collector'
+import { loadAllRules } from './../src/ia/utils/guideline.collector'
 
 // @see https://github.com/chroma-core/chroma
 import { ChromaClient } from 'chromadb'
@@ -8,15 +7,14 @@ import { QUERIES } from './queries.utils'
 
 const main = async () => {
   const rootNode = await collectOnlineGuidelines()
-  const tsCodingGuidelines: GuidelineNode = rootNode.children[0] // 1st one is TS coding guidelines
-  const rules = loadRules(tsCodingGuidelines)
+  const rules = loadAllRules(rootNode)
 
   const client = new ChromaClient()
   const collection = await client.getOrCreateCollection({ name: 'codingGuidelinesCollection' })
 
   await collection.upsert({
     documents: rules.map((rule) => rule.content),
-    ids: rules.map((rule) => rule.title),
+    ids: rules.map((rule, index) => `${index + 1}-${rule.title}`),
     metadatas: rules.map((rule) => ({ title: rule.title })),
   })
 
--- coding-guide-helper/ollama-integration/queries.utils.ts
+++ coding-guide-helper/ollama-integration/queries.utils.ts
@@ -1,1 +1,6 @@
-export const QUERIES = ['ternary abuse', 'many args position', 'multiple values comparison']
+export const QUERIES = [
+  'ternary abuse',
+  'many args position',
+  'multiple values comparison',
+  'if then else complex React component',
+]
