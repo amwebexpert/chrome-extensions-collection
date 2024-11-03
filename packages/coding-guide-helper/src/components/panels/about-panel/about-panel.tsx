5a071b64794605aa5c20538ef7445101cc230df0
Andre-3rd-Viking <andre@t-b.ca>
2024-11-03 17:21:10 -0500

    feat: added repo url and logo

--- packages/coding-guide-helper/src/components/panels/about-panel/about-panel.tsx
+++ packages/coding-guide-helper/src/components/panels/about-panel/about-panel.tsx
@@ -1,7 +1,69 @@
 import { Table } from 'antd'
 import type { FunctionComponent } from 'react'
-import { ABOUT_INFOS, COLUMNS } from './about-panel.utils'
+import { APP_VERSION_INFO } from '../../../constants'
+import { getManifestData } from '../../app/app.utils'
+import './about-panel.css'
+
+type AboutInfo = {
+  key: string
+  label: string
+  value: React.ReactNode
+}
+
+const manifestData = getManifestData()
+
+const ABOUT_INFOS: AboutInfo[] = [
+  {
+    key: 'name',
+    label: 'Extension Name',
+    value: manifestData.name,
+  },
+  {
+    key: 'description',
+    label: 'Description',
+    value: APP_VERSION_INFO.DESCRIPTION,
+  },
+  {
+    key: 'version',
+    label: 'Version',
+    value: APP_VERSION_INFO.VERSION,
+  },
+  {
+    key: 'author',
+    label: 'Author',
+    value: APP_VERSION_INFO.AUTHOR,
+  },
+  {
+    key: 'buildDate',
+    label: 'Last Build Date',
+    value: APP_VERSION_INFO.VERSION_DATE,
+  },
+]
+
+const COLUMNS = [
+  {
+    title: 'Info',
+    dataIndex: 'label',
+    key: 'label',
+    width: '25%',
+  },
+  {
+    title: 'Details',
+    dataIndex: 'value',
+    key: 'value',
+    width: '75%',
+  },
+]
+
+const iconURL =
+  'https://raw.githubusercontent.com/amwebexpert/chrome-extensions-collection/master/packages/coding-guide-helper/public/icons/icon-48.png'
 
 export const AboutPanel: FunctionComponent = () => (
-  <Table columns={COLUMNS} dataSource={ABOUT_INFOS} pagination={false} size="small" bordered={true} />
+  <div className="about-panel">
+    <div className="about-panel-image-container">
+      <img src={iconURL} alt={APP_VERSION_INFO.NAME} />
+    </div>
+
+    <Table columns={COLUMNS} dataSource={ABOUT_INFOS} pagination={false} size="small" bordered={true} />
+  </div>
 )
