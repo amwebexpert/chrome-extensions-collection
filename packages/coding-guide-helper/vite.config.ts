212269f42ef0c35090443bdce74947973eb2e3a4
Andre-3rd-Viking <andre@t-b.ca>
2024-11-06 07:23:30 -0500

    chore: remove viteManifestHackIssue846 hack

--- packages/coding-guide-helper/vite.config.ts
+++ packages/coding-guide-helper/vite.config.ts
@@ -1,23 +1,13 @@
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react'
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import version from "vite-plugin-package-version";
+import version from 'vite-plugin-package-version'
 import { manifest } from './manifest'
-
-const viteManifestHackIssue846 = {
-  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-  name: 'manifestHackIssue846',
-  renderCrxManifest(_manifest, bundle) {
-      bundle['manifest.json'] = bundle['.vite/manifest.json']
-      bundle['manifest.json'].fileName = 'manifest.json'
-      delete bundle['.vite/manifest.json']
-  },
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), version(), viteManifestHackIssue846, crx({ manifest })],
+  plugins: [react(), version(), crx({ manifest })],
   server: {
     port: 5173,
     strictPort: true,
