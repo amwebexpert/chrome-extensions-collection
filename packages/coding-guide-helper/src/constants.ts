331c8cddecc45ccb41b6a894df8893b48f53b4e8
Andre-3rd-Viking <andre@t-b.ca>
2024-11-29 15:30:44 -0600

    chore: setup 1.1.0

--- packages/coding-guide-helper/src/constants.ts
+++ packages/coding-guide-helper/src/constants.ts
@@ -7,8 +7,8 @@ export const APP_VERSION_INFO = Object.freeze({
   DESCRIPTION: "Coding guide helper is a chrome extension to help doing pull requests review process.",
   REPOSITORY: 'https://github.com/amwebexpert/chrome-extensions-collection/tree/master/packages/coding-guide-helper',
   AUTHOR: 'amwebexpert@gmail.com',
-  VERSION: '1.0.9',
+  VERSION: '1.1.0',
   VERSION_DATE: '2024-11-29',
 });
 
-export const LONG_VERSION_DATE = '1.0.9 (2024-11-29)';
+export const LONG_VERSION_DATE = '1.1.0 (2024-11-29)';
